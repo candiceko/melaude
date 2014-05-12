da56ec124c1210029bcc25c62040f0f006fa3a88
Kevin Sison <kevinasison@gmail.com>
2014-05-11 17:22:41 -0700

    Re-added mean.io files and changed database to MongoHQ

--- public/system/services/global.js
+++ public/system/services/global.js
@@ -2,17 +2,14 @@
 
 //Global service for global variables
 angular.module('mean.system').factory('Global', [
+
     function() {
         var _this = this;
         _this._data = {
             user: window.user,
-            authenticated: !! window.user,
-            roles: window.userRoles,
-            hasRole : function(role) { 
-            	return (role == 'annonymous' || window.roles.indexOf('admin') != -1 || window.roles.indexOf(role) != -1)
-            }
+            authenticated: window.user && window.user.roles,
+            isAdmin: (window.user && window.user.roles) && window.user.roles.indexOf('admin') > -1
         };
-
         return _this._data;
     }
 ]);
