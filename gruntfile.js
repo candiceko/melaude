4d303399bfe411cbd77fdf934bb644fc337ff3cb
Kevin Sison <kevinasison@gmail.com>
2014-05-11 18:33:52 -0700

    Updated heroku grunt task

--- gruntfile.js
+++ gruntfile.js
@@ -122,5 +122,6 @@ module.exports = function(grunt) {
 
     // For Heroku users only.
     // Docs: https://github.com/linnovate/mean/wiki/Deploying-on-Heroku
-    grunt.registerTask('heroku:production', ['cssmin', 'uglify']);
+    // grunt.registerTask('heroku:production', ['cssmin', 'uglify']);
+    grunt.registerTask('heroku:production', ['clean','jshint', 'csslint', 'concurrent']);
 };
