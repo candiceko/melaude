7f799349b82c6567f844434dbf17a2a9b90cd362
Kevin Sison <kevinasison@gmail.com>
2014-05-11 19:15:08 -0700

    Added test and production grunt configs

--- gruntfile.js
+++ gruntfile.js
@@ -122,5 +122,6 @@ module.exports = function(grunt) {
 
     // For Heroku users only.
     // Docs: https://github.com/linnovate/mean/wiki/Deploying-on-Heroku
-    grunt.registerTask('heroku:production', ['clean','jshint', 'csslint', 'concurrent']);
+    grunt.registerTask('heroku:test', ['clean','jshint', 'csslint', 'concurrent']);
+    grunt.registerTask('heroku:production', ['cssmin', 'uglify']);
 };
